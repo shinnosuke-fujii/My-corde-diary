29ba39eedd73c97343d103cb1a582d2ae020f492
shinnosuke-fujii <shinno934@gmail.com>
2023-09-22 21:39:52 +0900

    自由にわかりやすいメッセージ

--- db/migrate/20230823060728_add_user_id_to.rb
+++
@@ -1,4 +0,0 @@
-class AddUserIdTo < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20230825114353_remove_user_id_from_blogs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromBlogs < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :blogs, :user_id, :integer
-  end
-end
